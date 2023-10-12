a0a850d0a844ac0efb491c8d00e1f3336c764324
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2023-10-11 20:01:10 -0400

    Create Compare-Source-and-Destination-AD-Group-Add-Missing-Source-to-Destination.ps1

---
+++ Group-Membership/Compare-Source-and-Destination-AD-Group-Add-Missing-Source-to-Destination.ps1
@@ -0,0 +1,23 @@
+<# 
+
+PowerShell script that will compare a source AD group with a destination AD group and add the missing users from the source to the destination group
+
+#>
+
+# Define source and destination group variables to store group names
+$sourceGroup = "SourceGroup"
+$destinationGroup = "DestinationGroup"
+
+# Retrieve the users from the source group
+$sourceGroup = Get-ADGroupMember -Identity $sourceGroup
+
+# Retrieve the users from the destination group
+$destinationGroup = Get-ADGroupMember -Identity $destinationGroup
+
+# Store the users found in the source group and NOT the desintation group into variable
+$newUsers = $sourceGroup.SamAccountName | Where-Object { $destinationGroup.SamAccountName -notcontains $PSItem } | Sort-Object SamAccountName
+
+# Add missing users from Source to Destination group
+foreach ($newUser in $newUsers) {
+    Add-ADGroupMember -identity "DestinationGroup" -members $newUsers
+}
